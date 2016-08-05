f442060b23266d277bf1f2582bfd8d8ba619013b
Jeff.Tsai <wcai@maxleap.com>
2016-08-05 16:12:54 +0800

    add 8u102

--- build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-IMG="jjeffcaii/java"
-if [ -z "$1" ]; then
-    echo "usage: build.sh <version>"
-    exit 1
-fi
-
-docker build -f `dirname $0`/Dockerfile-$1 -t $IMG:$1 . && docker push $IMG:$1
